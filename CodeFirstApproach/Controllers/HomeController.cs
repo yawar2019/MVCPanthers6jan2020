8f8043544e9156cbce733b4d2fcf8788ca793b0c
yawar2019 <yawarali17@gmail.com>
2021-02-01 23:01:30 +0530

    committed Code

---
+++ CodeFirstApproach/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CodeFirstApproach.Models;
+namespace CodeFirstApproach.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        EmployeeContext db = new EmployeeContext();
+        public ActionResult Index()
+        {
+
+            return View(db.EmployeeModel.ToList());
+        }
+
+
+        public ActionResult Create()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(EmployeeModel emp)
+        {
+            db.EmployeeModel.Add(emp);
+            int i = db.SaveChanges();
+            if (i > 0)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ DatabaseFirstApproach/Controllers/DefaultController.cs
@@ -0,0 +1,20 @@
+﻿using DatabaseFirstApproach.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DatabaseFirstApproach.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+
+        EmployeeEntities db = new EmployeeEntities();
+        public ActionResult Index()
+        {
+            return View(db.employeeDetails.ToList());
+        }
+    }
+}
